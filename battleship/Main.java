a3a513b0a516a9d88964f13dc7669f3d9e6c638b
Isaul <tiradolisaul@gmail.com>
2024-08-14 15:09:25 -0700

    Modification on Main

--- battleship/Main.java
+++ battleship/Main.java
@@ -8,6 +8,23 @@ public class Main {
         GameController gameController = new GameController();
         gameController.makeFleet();
 
+        /**
+         * Stage 3/6: Adding shooting functionality
+         */
+        /*
+        System.out.println("\n The game starts! \n");
+        gameController.getTable().displayTable();
+        System.out.println("\n Take a shot! \n");
+        gameController.takeAShot();
+         */
+
+        /**
+         * Stage 4/6:  Implement the "fog of war" feature
+         */
+        System.out.println("\n The game starts! \n");
+        gameController.getGameTable().displayTable();
+        System.out.println("\n Take a shot! \n");
+        gameController.takeAShot();
     }
 }
 
