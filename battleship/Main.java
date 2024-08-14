36189cd0dbd860fcf6a43497894fe86f4212f02f
Isaul <tiradolisaul@gmail.com>
2024-08-13 20:26:41 -0700

    feature: Fog of War implemented -> Completed

--- battleship/Main.java
+++ battleship/Main.java
@@ -11,11 +11,20 @@ public class Main {
         /**
          * Stage 3/6: Adding shooting functionality
          */
+        /*
         System.out.println("\n The game starts! \n");
         gameController.getTable().displayTable();
         System.out.println("\n Take a shot! \n");
         gameController.takeAShot();
+         */
 
+        /**
+         * Stage 4/6:  Implement the "fog of war" feature
+         */
+        System.out.println("\n The game starts! \n");
+        gameController.getGameTable().displayTable();
+        System.out.println("\n Take a shot! \n");
+        gameController.takeAShot();
     }
 }
 
